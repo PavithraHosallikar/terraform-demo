2cb9180e496118bd810e80a6f67d3fc97b5c1739
Pavithra Hosallikar <pavithrahosallis@gmail.com>
2025-08-07 13:39:14 +0530

    Adding new resource to test the workflow

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+
+resource "azurerm_storage_account" "demo" {
+  name                     = "demostorage${random_integer.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_integer" "suffix" {
+  min = 10000
+  max = 99999
+}
